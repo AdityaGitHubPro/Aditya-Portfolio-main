e8c3086c931c282aca0d77888dce2293c8ff20fe
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-02-02 10:21:44 +0530

    Add files via upload

---
+++ Aditya-Portfolio-main/main.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const dynamicContent = document.getElementById("dynamic-text");
+
+    const phrases = ["Software Engineer...", "Student...", "Human Being..."]
+    let pharseIndex = 0;
+    let letterIndex = 0;
+    const typingSpeed = 150;
+    const erasingSpeed = 75
+
+    function printLetters(phrase) {
+
+        if (letterIndex == phrase.length) {
+            clearLetters();
+        }
+        else if (letterIndex < phrase.length) {
+            dynamicContent.textContent += phrase.charAt(letterIndex);
+            letterIndex += 1;
+            setTimeout(function () {
+                printLetters(phrase)
+            }, typingSpeed)
+        }
+    }
+
+    function clearLetters() {
+        if (letterIndex == -1) {
+            pharseIndex = (pharseIndex + 1) % phrases.length;
+            letterIndex = 0;
+            printLetters(phrases[pharseIndex])
+        }
+        else if (letterIndex > -1) {
+            let updatedPhrase = "";
+            for (let index = 0; index < letterIndex; index++) {
+                updatedPhrase += phrases[pharseIndex].charAt(index);
+
+            }
+            dynamicContent.textContent = updatedPhrase;
+            letterIndex -= 1;
+            setTimeout(clearLetters, erasingSpeed)
+        }
+    }
+
+    printLetters(phrases[pharseIndex]);
+
+    window.addEventListener("scroll", function () {
+        console.log(this.window.scrollY)
+        let intro = this.document.querySelector(".intro");
+        if (this.window.scrollY >= (intro.offsetHeight + intro.offsetTop)) {
+            this.document.querySelector(".header").style.position = "sticky";
+        } else {
+            this.document.querySelector(".header").style.position = "revert";
+
+        }
+    })
+})
+
